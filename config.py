58dfd9fccac27d2c0f10536cdffab57ca171b044
Arash Hatami <hatamiarash7@gmail.com>
2023-04-02 21:37:15 +0330

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = os.environ.get('DEBUG', False)
-HOST = os.environ.get('HOST', 'localhost')
-PORT = int(os.environ.get('PORT', 8080))
-
-LOGGING_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOGGING_LOCATION = 'parser.log'
-LOGGING_LEVEL = 'DEBUG'
