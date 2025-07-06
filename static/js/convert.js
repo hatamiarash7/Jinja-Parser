7f7c18e6915be6e648de26dcf25632d5458f04e9
Arash Hatami <hatamiarash7@gmail.com>
2025-07-06 18:22:50 +0330

    chore: format js file and use new return var

--- static/js/convert.js
+++ static/js/convert.js
@@ -1,25 +1,28 @@
 $(document).ready(function () {
-    $('#clear').click(function () {
-        $('#template').val('');
-        $('#render').val('');
-        $('#values').val('');
-        $('#render').html('');
-    });
+  $("#clear").click(function () {
+    $("#template").val("");
+    $("#render").val("");
+    $("#values").val("");
+    $("#render").html("");
+  });
 
-    $('#convert').click(function () {
-        var whitespaces = $('input[name="whitespace"]').is(':checked') ? 1 : 0;
-        var dummy = $('input[name="dummy"]').is(':checked') ? 1 : 0;
-        var input_type = $('input[name="type"]:checked').val();
+  $("#convert").click(function () {
+    var whitespaces = $('input[name="whitespace"]').is(":checked") ? 1 : 0;
+    var dummy = $('input[name="dummy"]').is(":checked") ? 1 : 0;
+    var input_type = $('input[name="type"]:checked').val();
 
-        $.post('/convert', {
-            template: $('#template').val(),
-            values: $('#values').val(),
-            type: input_type,
-            whitespaces: whitespaces,
-            dummy: dummy
-        }).done(function (response) {
-            response = response.replace(/•/g, '<span class="whitespace">•</span>');
-            $('#render').html(response);
-        });
+    $.post("/convert", {
+      template: $("#template").val(),
+      values: $("#values").val(),
+      type: input_type,
+      whitespaces: whitespaces,
+      dummy: dummy,
+    }).done(function (response) {
+      response = response.rendered_output.replace(
+        /•/g,
+        '<span class="whitespace">•</span>'
+      );
+      $("#render").html(response);
     });
+  });
 });
